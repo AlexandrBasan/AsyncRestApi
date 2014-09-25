2c46c38ed563930827a443d91037ef3b8d5dd094
Tyler Ewing <tewing10@gmail.com>
2014-09-25 07:00:41 -0400

    Add DB seeds and remove fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Job.create!(name: 'Check for face', algorithm: 'facial recognition')
+Job.create!(name: 'Make into avatar', algorithm: 'resize')
