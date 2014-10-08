76ac92b9e304b55f1e88ec0f363738ff4b66f821
Tyler Ewing <tewing10@gmail.com>
2014-10-07 21:10:19 -0400

    Update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 Job.delete_all
-Job.create!(name: 'Check for face', algorithm: 'facial recognition')
-Job.create!(name: 'Make into avatar', algorithm: 'resize')
+Job.create!(name: 'Check for face', algorithm: 'facial recognition', status: 'completed')
+Job.create!(name: 'Make into avatar', algorithm: 'resize', status: 'queued')
+Job.create!(name: 'Invert image', algorithm: 'invert', status: 'queued')
